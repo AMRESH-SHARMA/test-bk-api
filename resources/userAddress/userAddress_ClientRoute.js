38e4c12e46223d84bf29ae91a612f643e6039784
AMRESH <amreshaiio@gmail.com>
2023-02-08 01:03:19 +0530

    'ok'

--- resources/userAddress/userAddress_ClientRoute.js
+++ resources/userAddress/userAddress_ClientRoute.js
@@ -5,6 +5,7 @@ import {
   addUserAddressClient,
   getAllAddressClient,
   deleteSingleAddressClient,
+  updateUserAddress
 } from "./userAddressController.js"
 import { isAuthenticated } from "../../util/auth.js"
 import { vAccessToken } from "../../util/validators.js"
@@ -12,6 +13,8 @@ import { vAccessToken } from "../../util/validators.js"
 router.route("/user/create-address").post(vAccessToken, isAuthenticated, addUserAddressClient);
 
 router.route("/user/get-address").get(vAccessToken, isAuthenticated, getAllAddressClient);
+
+router.route("/user/update-address/:addressId").put(vAccessToken, isAuthenticated, updateUserAddress);
 
 router.route("/user/delete-single-address/:addressId").delete(deleteSingleAddressClient);
 
