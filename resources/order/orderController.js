0884b5d49518bf2b19908b57a15cdc161c8d8e6b
AMRESH <amreshaiio@gmail.com>
2023-02-23 16:48:10 +0530

    'ok'

--- resources/order/orderController.js
+++ resources/order/orderController.js
@@ -76,7 +76,6 @@ export const getOrdersByUserId = async (req, res, next) => {
     });
 
     sendResponse(200, true, { totalDocs, result: orders }, res);
-    // sendResponse(200, true, { totalDocs, result: userOrder }, res);
   } catch (e) {
     console.log(e);
     sendResponse(400, false, e.message, res)
