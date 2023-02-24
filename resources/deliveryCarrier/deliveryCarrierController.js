39c8f627c5a2e88a8cb937470cebe6ab1b033fdf
AMRESH <amreshaiio@gmail.com>
2023-02-24 17:08:13 +0530

    'ok'

--- resources/deliveryCarrier/deliveryCarrierController.js
+++ resources/deliveryCarrier/deliveryCarrierController.js
@@ -8,7 +8,6 @@ import { mediaDel } from "../../util/mediaDel.js"
 export const createDeliveryCarrier = async (req, res, next) => {
   try {
     const { uniqueId, carrierName, email, phone, address } = req.body;
-    console.log(req.body);
     const exist1 = await DeliveryCarrier.findOne({ carrierName: carrierName }).countDocuments();
     if (exist1) {
       return sendResponse(400, false, 'carrier name already in use', res)
@@ -19,13 +18,27 @@ export const createDeliveryCarrier = async (req, res, next) => {
       return sendResponse(409, false, 'email already in use', res)
     }
 
-    const newData = await DeliveryCarrier.create({
+    let payLoadObj = {
       _id: uniqueId,
       carrierName,
       email,
       phone,
       address
-    });
+    }
+
+    if (req.file) {
+      await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: "deliveryCarrier/",
+      }).then((result) => {
+        payLoadObj.image = {
+          public_id: result.public_id,
+          url: result.url,
+        }
+        mediaDel()
+      })
+    }
+
+    const newData = await DeliveryCarrier.create(payLoadObj);
     sendResponse(201, true, newData, res)
   } catch (e) {
     console.log(e);
@@ -85,235 +98,46 @@ export const updateStatus = async (req, res, next) => {
   }
 };
 
-// **************************************************
-
-// 4.Forgot Password
-// export const forgotPassword = async (req, res, next) => {
-//   const user = await User.findOne({ email: req.body.email });
-
-//   if (!user) {
-//     return sendResponse(404, false, 'User not found', res)
-//   }
-
-//   try {
-//     // Get ResetPassword Token
-//     const resetToken = user.getResetPasswordToken();//call function
-
-//     //save database reset token
-//     await user.save({ validateBeforeSave: false });
-//     //create link for send mail
-//     // const resetPasswordUrl = `http://localhost:5000/api/v1/user/password/reset/${resetToken}` //send from localhost
-//     //send from anyhost
-//     // const resetPasswordUrl = `${req.protocol}://${req.get(
-//     //     "host"
-//     // )}/api/v1/user/password/reset/${resetToken}`;
-//     //const resetPasswordUrl = `${process.env.FRONTEND_URL}:/api/user/password/reset/${resetToken}`;
-//     //const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
-//     const password = generator.generate({
-//       length: 10,
-//       numbers: true
-//     });
-//     user.password = password;
-//     await user.save()
-//     // const message = `Your password reset token are :- \n\n ${resetPasswordUrl} \n\nyour new password is:${password}\n\nIf you have not requested this email then, please ignore it.`;
-//     // await sendEmail({
-
-//     //   to: `${user.email}`, // Change to your recipient
-//     //   from: 'project.edufuture@gmail.com', // Change to your verified sender
-//     //   subject: `CMP Password Recovery`,
-//     //   html: `your new password is: <br/> <strong> ${password}</strong><br/><br/>If you have not requested this email then, please ignore it.`
-
-//     // });
-//     console.log(resetToken);
-//     // res.status(200).json({
-//     //   success: true,
-//     //   message: `Email sent to ${user.email} successfully`,
-//     // });
-//     sendResponse(200, true, resetToken, res)
-//   } catch (e) {
-//     user.resetPasswordToken = undefined;
-//     user.resetPasswordExpire = undefined;
-
-//     await user.save({ validateBeforeSave: false });
-
-//     return sendResponse(500, false, e.message, res);
-//   }
-// };
-
-
-// // 5.Reset Password
-// export const resetPassword = catchAsyncErrors(async (req, res, next) => {
-//   // creating token hash
-//   const resetPasswordToken = crypto
-//     .createHash("sha256")
-//     .update(req.params.token)
-//     .digest("hex");
-
-//   const user = await User.findOne({
-//     resetPasswordToken,
-//     resetPasswordExpire: { $gt: Date.now() },
-//   });
-
-//   if (!user) {
-//     return next(
-//       new ErrorHander(
-//         "Reset Password Token is invalid or has been expired",
-//         400
-//       )
-//     );
-//   }
-//   //replace previous password
-//   if (req.body.password !== req.body.confirmPassword) {
-//     return next(new ErrorHander("Password does not password", 400));
-//   }
-
-//   user.password = req.body.password;
-//   user.resetPasswordToken = undefined;
-//   user.resetPasswordExpire = undefined;
-
-//   await user.save();
-
-//   sendToken(user, 200, res);
-// });
-
-
-// // 8.update User password
-// export const updatePassword = catchAsyncErrors(async (req, res, next) => {
-//   const user = await User.findById(req.user.id).select("+password");
-
-//   const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
-
-//   if (!isPasswordMatched) {
-//     return next(new ErrorHander("Old password is incorrect", 400));
-//   }
-
-//   if (req.body.newPassword !== req.body.confirmPassword) {
-//     return next(new ErrorHander("password does not match", 400));
-//   }
-
-//   user.password = req.body.newPassword;
-
-//   await user.save();
-
-//   sendToken(user, 200, res);
-// });
-
-// 9.update User Profile
-// export const updateProfile = async (req, res, next) => {
-//   const newUserData = {
-//     name: req.body.name,
-//     phone: req.body.phone,
-//     email: req.body.email,
-//   };
-
-//   if (req.files) {
-//     const files = req.files.avatar;
-//     const user = await User.findById(req.user.id);
-
-//     const imageId = user.avatar.public_id;
-
-//     await cloudinary.uploader.destroy(imageId)
-
-//     const myCloud = await cloudinary.uploader.upload(files.tempFilePath, {
-//       folder: "image",
-//     },
-//       function (error, result) { (result, error) });
-
-//     newUserData.avatar = {
-//       public_id: myCloud.public_id,
-//       url: myCloud.secure_url,
-//     };
-//   }
-//   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
-//     new: true,
-//     runValidators: true,
-//     useFindAndModify: false,
-//   });
-
-//   sendResponse(200, true, user, res)
-// };
-
-// // 9.Get all users(admin)
-// export const getAllUser = catchAsyncErrors(async (req, res, next) => {
-
-//   const users = await User.find()//.select('-role');
-
-//   res.status(200).json({
-//     success: true,
-//     users,
-//   });
-// });
-
-//Get single user
-
-
-//Get Books Uploaded By Single User By Param
-
-export const getBooksUploadedBySingleUserByParam = async (req, res, next) => {
+// update
+export const updateDeliveryCarrier = async (req, res, next) => {
   try {
-
-    const userId = req.params.id;
-
-    const user = await User.findById(userId).select('booksAdded').populate({
-      path: 'booksAdded',
-      populate: { path: 'genre language' }
-    });
-
-    if (!user) {
-      return sendResponse(400, false, `User does not exist with Id: ${userId}`, res)
+    const { carrierId } = req.params
+    const dc = await DeliveryCarrier.findById(carrierId);
+    if (req.file) {
+      await cloudinary.v2.uploader.destroy(dc.image.public_id);
+      await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: "deliveryCarrier/",
+      }).then((result) => {
+        req.body.image = {
+          public_id: result.public_id,
+          url: result.url,
+        }
+        mediaDel()
+      })
     }
-    sendResponse(200, true, user, res)
+
+    await DeliveryCarrier.findByIdAndUpdate(carrierId, req.body);
+    sendResponse(200, true, 'Updated Successfully', res)
+  } catch (e) {
+    console.log(e);
+    if (e.code) {
+      return sendResponse(400, false, `${Object.keys(e.keyValue)} already in use`, res)
+    }
+    sendResponse(400, false, e, res)
+  }
+};
+
+// delete
+export const deleteDeliveryCarrier = async (req, res, next) => {
+  try {
+    const { carrierId } = req.params;
+    const dc = await DeliveryCarrier.findById(carrierId);
+
+    await cloudinary.v2.uploader.destroy(dc.image.public_id)
+    await DeliveryCarrier.deleteOne({ _id: carrierId });
+    sendResponse(201, true, 'deleted', res)
   } catch (e) {
     console.log(e);
     sendResponse(400, false, e.message, res)
   }
-};
-
-//Get Books Uploaded By Single User
-export const getBooksUploadedBySingleUser = async (req, res, next) => {
-  try {
-
-    const userId = req.authTokenData.id;
-
-    const user = await User.findById(userId).where('approved').equals(true).select('booksAdded').populate({
-      path: 'booksAdded',
-      populate: { path: 'genre language' }
-    });
-
-    if (!user) {
-      return sendResponse(400, false, `User does not exist with Id: ${userId}`, res)
-    }
-    sendResponse(200, true, user, res)
-  } catch (e) {
-    console.log(e);
-    sendResponse(400, false, e.message, res)
-  }
-};
-
-//Update user by param
-// export const updateUserByParam = async (req, res, next) => {
-//   try {
-
-//     const userId = req.params.id
-
-//     if (req.file) {
-//       await cloudinary.v2.uploader.upload(req.file.path, {
-//         folder: "user/",
-//       }).then((result) => {
-//         req.body.image = {
-//           public_id: result.public_id,
-//           url: result.url,
-//         }
-//         mediaDel()
-//       })
-//     }
-
-//     await User.findByIdAndUpdate(userId, req.body);
-//     sendResponse(200, true, 'Updated Successfully', res)
-//   } catch (e) {
-//     if (e.code) {
-//       return sendResponse(400, false, `${Object.keys(e.keyValue)} already in use`, res)
-//     }
-//     sendResponse(400, false, e, res)
-//   }
-// };
+}
--- resources/deliveryCarrier/deliveryCarrierRoute.js
+++ resources/deliveryCarrier/deliveryCarrierRoute.js
@@ -4,13 +4,8 @@ import multer from "multer";
 const upload = multer({ dest: 'uploads/' })
 
 import {
-  // logout,
-  // forgotPassword,
-  // resetPassword,
-  // updatePassword,
-  // updateProfile,
-  // updateUserByParam,
-  // getBooksUploadedBySingleUserByParam,
+  updateDeliveryCarrier,
+  deleteDeliveryCarrier,
   getAllCarrier,
   getSingleCarrier,
   createDeliveryCarrier,
@@ -21,33 +16,14 @@ import { vAccessToken } from "../../util/validators.js"
 
 router.route("/deliveryCarrier").get(vAccessToken, isAuthenticatedAdmin, getAllCarrier);
 
-router.route("/deliveryCarrier").post(vAccessToken, isAuthenticatedAdmin, createDeliveryCarrier);
+router.route("/deliveryCarrier").post(vAccessToken, isAuthenticatedAdmin, upload.single('image'), createDeliveryCarrier);
 
 router.route("/deliveryCarrier/:carrierId").get(vAccessToken, isAuthenticatedAdmin, getSingleCarrier);
 
 router.route("/deliveryCarrier").put(vAccessToken, isAuthenticatedAdmin, updateStatus);
-// ******************
 
-// router.route("/user/get-books-uploadedby-single-user/:id").get(vAccessToken, isAuthenticatedAdmin, getBooksUploadedBySingleUserByParam);
+router.route("/deliveryCarrier/:carrierId").put(upload.single('image'), updateDeliveryCarrier);
 
-// router.route("/user/update-user/:id").put(upload.single('image'), updateUserByParam);
-
-// router.route("/user/forgot-password").post(forgotPassword);
-
-// router.route("/user/password/reset/:token").put(resetPassword);
-
-// router.route("/user/details").get(isAuthenticatedAdminUser, getUserDetails);
-// router
-//     .route("/admin/users")
-//     .get(isAuthenticatedAdminUser, authorizeRoles("admin"), getAllUser);
-// router
-//     .route("/admin/user/:id")
-//     .get(isAuthenticatedAdminUser, authorizeRoles("admin"), getSingleUser)
-
-
-
-// router.route("/user/password/update").put(isAuthenticatedAdminUser, updatePassword);
-
-// router.route("/user/update-profile").put(vAccessToken, isAuthenticatedAdminUser, updateProfile);
+router.route("/deliveryCarrier/:carrierId").delete(vAccessToken, isAuthenticatedAdmin, deleteDeliveryCarrier);
 
 export default router;
