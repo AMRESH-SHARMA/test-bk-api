2a22a03b112c531259d4072732ce0a62b4a2c784
AMRESH <amreshaiio@gmail.com>
2023-03-03 00:47:48 +0530

    'ok'

--- resources/book/bookModel.js
+++ resources/book/bookModel.js
@@ -83,16 +83,13 @@ const bookSchema = new mongoose.Schema({
   city: {
     type: String,
     trim: true,
-    // required: true,
     maxLength: 20,
-    unique: true,
   },
   state: {
     type: String,
     trim: true,
     // required: true,
     maxLength: 20,
-    unique: true,
   },
 }, { timestamps: true });
 
